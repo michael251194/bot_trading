5fda143eed245e4ef6a501c1b1e499a9f63560b7
MichaelT94 <112983215+michael251194@users.noreply.github.com>
2023-01-27 19:45:32 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ exchange = ccxt.binance({
 })
 
 # Récupère les données de prix en temps réel pour le Bitcoin
-ticker = exchange.fetch_ticker('BTC/USDT')
+ticker = exchange.fetch_ticker('GMT/USDT')
 
 # Définir les paramètres de la stratégie de scalping
 take_profit = 0.5 # En pourcentage
@@ -33,7 +33,7 @@ lot_size = 0.01 # Taille de la position
 # Boucle infinie pour surveiller le marché en temps réel
 while True:
     # Récupère les données de prix en temps réel
-    ticker = exchange.fetch_ticker('BTC/USDT')
+    ticker = exchange.fetch_ticker('GMT/USDT')
     last_price = ticker['last']
     bid_price = ticker['bid']
     ask_price = ticker['ask']
@@ -41,9 +41,9 @@ while True:
     # Vérifie si le prix a augmenté de plus de X%
     if last_price >= (1 + take_profit/100) * bid_price:
         # Place un ordre de vente
-        order = exchange.create_order('BTC/USDT', 'limit', 'sell', lot_size, last_price)
+        order = exchange.create_order('GMT/USDT', 'limit', 'sell', lot_size, last_price)
 
     # Vérifie si le prix a diminué de plus de X%
     elif last_price <= (1 - stop_loss/100) * ask_price:
         # Place un ordre d'achat
-        order = exchange.create_order('BTC/USDT', 'limit', 'buy', lot_size, last_price)
+        order = exchange.create_order('GMT/USDT', 'limit', 'buy', lot_size, last_price)
